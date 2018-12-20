2951452fdb22bff3d9f509e22ee3e6719c9381b1
Alim Buğra Çınar <abugracinar@gmail.com>
2018-12-20 19:40:08 +0300

    best rps 0.1683

--- main.r
+++ main.r
@@ -16,13 +16,14 @@ require(xgboost)
 require(e1071)
 require(Ckmeans.1d.dp)
 
-
+setwd("C:/Users/Bugra/Documents/GitHub/match_mining")
 source('data_preprocessing.r')
 source('feature_extraction.r')
 source('performance_metrics.r')
 source('train_models.r')
 ### match_processing is for adding average goals, average scores and days before the match
 source('match_processing.r')
+
 
 #save paths
 matches_data_path = "Files/df9b1196-e3cf-4cc7-9159-f236fe738215_matches.rds"
@@ -94,14 +95,25 @@ colnames(additional_data) <- col_names
 
 
 comp_data <- merge(matches, additional_data, by.x = c("Match_Date", "Home", "Away"), by.y  = c("Date", "Home", "Away"), all.x = TRUE)
-avg_days <- 3
+avg_days <- 5
 avg_method <- "w"
 
+days <- c(2,3,4)
+methods <- c("s", "t", "w")
+i <- 2
+j <- "t"
+bugra_results <- data.table()
 
-
+for(i in days)
+{
+  for(j in methods)
+  {
+    
 
 ## Add extra features to matches (winning average, score average, days before the match)
-time <- system.time(matches <- match_processing(comp_data, avg_days, avg_method) )
+time <- system.time(matches <- match_processing(comp_data, i, j) )
+
+
 
 # preprocess odd data
 odd_details=details_data_preprocessing(odd_details_raw,matches,which_bets = c("1x2"))
@@ -122,9 +134,9 @@ cbind(names(train_features), c(1:ncol(train_features)))
 
 #Seperate Results and Data, remove matchID, MatchDate and LeagueID columns
 trainclass <- train_features$Match_Result
-traindata <- train_features[,c(3:5,7:42,43,60,77,95,112,129)]
+traindata <- train_features[,-c(1,2,6)]
 testclass <- test_features$Match_Result
-testdata <- test_features[,c(3:5,7:42,43,60,77,95,112,129)]
+testdata <- test_features[,-c(1,2,6)]
 
 #Results as numeric values
 trainclass <- (trainclass == "Home")*1 + (trainclass == "Away")*2
@@ -136,65 +148,26 @@ results[1,] <- (testclass == 1)*1
 results[2,] <- (testclass == 0)*1
 results[3,] <- (testclass == 2)*1
 
+g <- 19
+k <- 106
 
+for (g in c(19,21,23,25))
+{
+for(k in c(106,142, 160:180))
+{
+  set.seed(k)
+  
 cols <- names(traindata)
-
-cbind(names(traindata), c(1:ncol(traindata)))
-cols <- cols[c(9:45)]
-
-# Inputs are generated from data files
-train1 <- traindata[,..cols]
-test1 <- testdata[,..cols]
-#Inputs are scaled
-train1 <- as.data.table(scale(train1))
-test1 <- as.data.table(scale(test1))
-
-
-#knn model is constructed. k is determined arbitrarily, no cross validation
-pred11 <- knn(train1,test1, trainclass, k = 29, prob = TRUE)
-
-#confusion matrix and accuracy
-confusion_matrix_knn <- table(pred11,testclass)
-accuracy_knn <- sum(pred11==testclass)/length(testclass)
-
-
-# Bind train and test data to be used as an input in KODAMA's knn.dist function
-x <- rbind(train1,test1)
-
-#Distances are calculated
-kdist <- KODAMA::knn.dist(x)
-
-#Prediction is made
-pred <- KODAMA::knn.predict(1:nrow(train1), (nrow(train1)+1):nrow(x), trainclass, kdist, k=29, agg.meth = "majority")
-
-# display the confusion matrix and accuracy
-confusion_matrix_knn_kodama <- table(pred,testclass)
-accuracy_knn_kodama <- sum(pred==testclass)/length(testclass)
-accuracy_knn_kodama
-# view probabilities (all class probabilities are returned)
-prob <- KODAMA::knn.probability(1:nrow(train1), (nrow(train1)+1):nrow(x), trainclass, kdist, k=29)
-
-prob_rearranged <- prob
-prob_rearranged[1,] <- prob[2,]
-prob_rearranged[2,] <- prob[1,]
-
-# RPS Results are calculated
-  rps1mat <- RPS_matrix(t(prob_rearranged),t(results))
-rps1 <- mean(rps1mat)
-rps1
-#Output of RPS_Matrix function
-
-
-########## End of Nearest Neighbor Analysis
-
+index <- sample(c(5:ncol(traindata)), g)
+cols <- cols[index]
+sort(index)
+cbind(names(traindata), 1:(ncol(traindata)))
 ##### Model 2: Multinomial Regression
 
 #Model inputs determined
 train2 <- traindata[,..cols]
 test2 <- testdata[,..cols]
 
-train2 <- as.data.table(scale(train2))
-test2 <- as.data.table(scale(test2))
 #Multinomial model requires results to be in the data
 train2$Match_Result <- trainclass
 test2$Match_Result <- testclass
@@ -202,6 +175,7 @@ test2$Match_Result <- testclass
 #Model is generated
 multinomModel <- multinom(Match_Result ~ ., data=train2)
 summary (multinomModel)
+
 
 #Probabilities for each class and the results are generated
 predicted_scores <- predict (multinomModel, test2, "probs") # predict on new data
@@ -221,7 +195,64 @@ prob_rearranged[,2] <- predicted_scores[,1]
 rps2mat <- RPS_matrix(prob_rearranged, t(results))
 rps2 <- mean(rps2mat)
 rps2
+bugra_results <- rbind(bugra_results, data.table(day = i, method = j, sample = k, sample_no = g, time = time[1], accu = accuracy_multinom, rps = rps2))
+}}}}
 
+gulsah <- bugra_results[order(rps)]
+gulsah[1:50,]
+
+train1 <- traindata[,..cols]
+test1 <- testdata[,..cols]
+trainc1 <- as.factor((trainclass==1)*0 + (trainclass==0)*1+(trainclass==2)*2)
+testc1 <- as.factor((testclass==1)*0 + (testclass==0)*1+(testclass==2)*2)
+train1 <- cbind(train1,trainc1)
+
+############
+
+# Inputs are generated from data files
+cols <- 2:51
+train1 <- traindata[,..cols]
+test1 <- testdata[,..cols]
+train1 <- as.data.table(scale(train1))
+test1 <- as.data.table(scale(test1))
+
+
+#knn model is constructed. k is determined arbitrarily, no cross validation
+pred11 <- knn(train1,test1, trainclass, k = 95, prob = TRUE)
+
+#confusion matrix and accuracy
+confusion_matrix_knn <- table(pred11,testclass)
+accuracy_knn <- sum(pred11==testclass)/length(testclass)
+
+
+# Bind train and test data to be used as an input in KODAMA's knn.dist function
+x <- rbind(train1,test1)
+
+#Distances are calculated
+kdist <- KODAMA::knn.dist(x)
+
+#Prediction is made
+pred <- KODAMA::knn.predict(1:nrow(train1), (nrow(train1)+1):nrow(x), trainclass, kdist, k=95, agg.meth = "majority")
+
+# display the confusion matrix and accuracy
+confusion_matrix_knn_kodama <- table(pred,testclass)
+accuracy_knn_kodama <- sum(pred==testclass)/length(testclass)
+accuracy_knn_kodama
+# view probabilities (all class probabilities are returned)
+prob <- KODAMA::knn.probability(1:nrow(train1), (nrow(train1)+1):nrow(x), trainclass, kdist, k=95)
+
+prob_rearranged <- prob
+prob_rearranged[1,] <- prob[2,]
+prob_rearranged[2,] <- prob[1,]
+
+# RPS Results are calculated
+rps1mat <- RPS_matrix(t(prob_rearranged),t(results))
+rps1 <- mean(rps1mat)
+rps1
+#Output of RPS_Matrix function
+
+
+########## End of Nearest Neighbor Analysis
 
 #Inputs
 train4 <- traindata[,..cols]
@@ -234,7 +265,7 @@ train4$Match_Result <- as.factor(trainclass)
 test4$Match_Result <- as.factor(testclass)
 
 #Model is generated
-match.bagging <- bagging(Match_Result ~ ., data = train4)
+match.bagging <- bagging(Match_Result ~ ., data = train4,boos = TRUE, mfinal = 10, control = (minsplit = 0))
 
 #Predictions are made
 match.predbegging <- predict.bagging(match.bagging, newdata = test4)
@@ -327,81 +358,33 @@ rps6
 
 ########boosting
 
-train5 <- traindata[,..cols]
+train5 <- cbind(traindata[,..cols],trainclass)
 test5 <- testdata[,..cols]
-train5 <- as.matrix(scale(train5))
-test5 <- as.matrix(scale(test5))
-trainlabel <- as.matrix(trainclass)
-testlabel <- as.matrix(testclass)
 
-train_matrix <- xgb.DMatrix(data = train5, label = trainlabel)
-test_matrix <- xgb.DMatrix(data = test5, label = testlabel)
 
-numberOfClasses <- length(unique(testlabel))
-xgb_params <- list("objective" = "multi:softprob",
-                   "eval_metric" = "mlogloss",
-                   "num_class" = numberOfClasses)
-nround    <- 50 # number of XGBoost rounds
-cv.nfold  <- 5
+gbm.model <- gbm(trainclass~., data = train5, n.trees = 100, interaction.depth = 1,
+                        n.minobsinnode = 10, shrinkage =0.1, distribution = "multinomial")
+gbm.prob <- predict(gbm.model, test5, n.trees = 100, type = "response")
+gbm.pred <- apply(gbm.prob, 1, which.max)
+table(gbm.pred-1, testclass)
+sum((gbm.pred-1)==testclass)/nrow(test5)
 
-# Fit cv.nfold * cv.nround XGB models and save OOF predictions
-cv_model <- xgb.cv(params = xgb_params,
-                   data = train_matrix, 
-                   nrounds = nround,
-                   nfold = cv.nfold,
-                   verbose = FALSE,
-                   prediction = TRUE)
-cv_model$pred
-require(dplyr)
-OOF_prediction <- data.frame(cv_model$pred) %>%
-  mutate(max_prob = max.col(., ties.method = "last"),
-         label = trainlabel + 1)
-head(OOF_prediction)
+probboost <- as.data.frame(gbm.prob)
+probboost2 <- as.data.frame(gbm.prob)
+probboost[,1] <- probboost2[,2]
+probboost[,2] <- probboost2[,1]
 
-confusionMatrix(factor(OOF_prediction$max_prob),
-                factor(OOF_prediction$label),
-                mode = "everything")
-
-bst_model <- xgb.train(params = xgb_params,
-                       data = train_matrix,
-                       nrounds = nround)
-
-# Predict hold-out test set
-test_pred <- predict(bst_model, newdata = test_matrix)
-test_prediction <- matrix(test_pred, nrow = numberOfClasses,
-                          ncol=length(test_pred)/numberOfClasses) %>%
-  t() %>%
-  data.frame() %>%
-  mutate(label = testlabel + 1,
-         max_prob = max.col(., "last"))
-
-probs_boost <- test_prediction[,1:3]
-probs_boost[,1] <- test_prediction[,2]
-probs_boost[,2] <- test_prediction[,1]
-probs_boost[,3] <- test_prediction[,3]
-
-rps7 <- RPS_matrix(probs_boost, t(results))
+rps7 <- RPS_matrix(probboost, t(results))
 rps7 <- mean(rps7)
 rps7
-
-
-# confusion matrix of test set
-confusionMatrix(factor(test_prediction$max_prob),
-                factor(test_prediction$label),
-                mode = "everything")
-
-names <-  colnames(train5)
-# compute feature importance matrix
-importance_matrix = xgb.importance(feature_names = names, model = bst_model)
-head(importance_matrix)
 
 ####### support vector machines 
 
 
 train6 <- traindata[,..cols]
 test6 <- testdata[,..cols]
-trainlabel <- as.factor(trainlabel)
-testlabel <- as.factor(testlabel)
+trainlabel <- as.factor(trainclass)
+testlabel <- as.factor(testclass)
 
 train6 <- data.frame(train6,trainlabel)
 
